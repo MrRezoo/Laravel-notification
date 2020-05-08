ee861c10129ef53aa751b7156b9a131d3d2a7b01
Reza Mobaraki <rezam578@gmail.com>
2020-05-08 23:22:50 +0430

    language php file is Complete

--- resources/lang/fa/notification.php
+++ resources/lang/fa/notification.php
@@ -1,1 +1,16 @@
 <?php
+
+return [
+    'send_email' => 'ارسال ایمیل',
+    'user' => 'کاربر',
+    'email_type' => 'نوع ایمیل',
+    'send' => 'ارسال',
+    'cancel' => 'انصراف',
+    'email_sent_successfully' => 'ایمیل با موفقیت ارسال شد',
+    'email_has_problem'=>'سرویس ایمیل با مشکل مواجه شده است لطفا دقایقی بعد سعی کنید',
+    'sms_sent_successfully' => 'پیام کوتاه با موفقیت ارسال شد',
+    'sms_has_problem'=>'سرویس پیام کوتاه با مشکل مواجه شده است لطفا دقایقی بعد سعی کنید',
+    'sms_text' => 'متن پیام کوتاه',
+    'send_sms' => 'ارسال پیام کوتاه',
+    'notification.user_does_not_have_phone_number' => 'کاربر انتخابی فاقد شماره تلفن می‌باشد'
+];
--- resources/lang/fa/validation.php
+++ resources/lang/fa/validation.php
@@ -1,1 +1,20 @@
 <?php
+
+return [
+
+    'integer' => ':attribute باید به صورت عدد باشد',
+    'exists' => ':attribute باید به صورت صحیح وارد شده باشد ',
+
+    'string' => ':attribute باید به صورت صحیح وارد شود',
+    'max' => [
+        'string' => ':attribute حداکثر باید :max کارکتر باشد' ,
+
+    ] ,
+
+    'attributes' => [
+        'user' => 'کاربر',
+        'email_type' => 'نوع ایمیل',
+        'text' => 'متن پیام کوتاه'
+
+    ]
+];
